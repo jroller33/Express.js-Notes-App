9cf851f777f629e6b6dd3012168b3641c8451fff
John Roller <jtroller33@gmail.com>
2022-08-08 00:21:57 -0500

    .

---
+++ routes/landing.js
@@ -0,0 +1,7 @@
+const landing = require('express').Router();
+const { v4: uuidv4 } = require('uuid'); // not sure if I'll need this
+
+
+//  landing page
+
+module.exports = landing;
--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,6 @@
 const notes = require('express').Router();
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid'); // not sure if I'll need this
+
 const {
   readFromFile,
   readAndAppend,
