58aff219febfade9a2477faace80fce6a258ca67
John Roller <jtroller33@gmail.com>
2022-08-08 21:56:52 -0500

    testing without landing.js

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
-// import modular routers for /landing and /notes
-const landingRouter = require('./landing');
+// import modular routers forand /notes
+// landing.js not needed
+
+// const landingRouter = require('./landing');
 const notesRouter = require('./notes');
 
 const app = express();
 
-app.use('/landing', landingRouter);
+// app.use('/landing', landingRouter);
 app.use('/notes', notesRouter);
 
 module.exports = app;
--- routes/landing.js
+++ routes/landing.js
@@ -7,7 +7,7 @@
 
 
 // const landing = require('express').Router();
-// const { v4: uuidv4 } = require('uuid'); // not sure if I'll need this
+// const { v4: uuidv4 } = require('uuid'); 
 
 
 
