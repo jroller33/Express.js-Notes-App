4d89295cd0ef0be70c1787ec3d17e3c0b256332a
John Roller <jtroller33@gmail.com>
2022-08-08 19:56:27 -0500

    API routes almost done

--- README.md
+++ README.md
@@ -10,9 +10,9 @@
   to do:
   finish /routes/notes.js
   start testing routes with insomnia
+  UUID IS USED FOR NOTE IDS
   notes.html needs work
   make sure js that's dynamically changing html is working w notes.html
-  is uuid needed?
   connect everything
   have something deployed by tues/wed
   due sat
@@ -26,8 +26,8 @@ THEN I am presented with a landing page with a link to a notes page<br/>
 WHEN I click on the link to the notes page<br/>
 THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column<br/>
 WHEN I enter a new note title and the note’s text<br/>
-THEN a Save icon appears in the navigation at the top of the page<br/>
-WHEN I click on the Save icon<br/>
+THEN a Save icon appears in the  at the top of the page<br/>
+WHEN I click on the Save icon<br/>navigation
 THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes<br/>
 WHEN I click on an existing note in the list in the left-hand column<br/>
 THEN that note appears in the right-hand column<br/>
--- routes/notes.js
+++ routes/notes.js
@@ -13,10 +13,55 @@ notes.get('/', (req, res) => {
   });
 
 // GET route for specific note
+notes.get('/:note_id', (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.note_id === noteId);
+      return result.length > 0
+        ? res.json(result)
+        : res.json('No note with that ID');
+    });
+});
 
 // DELETE route for specific note
+notes.delete('/:note_id', (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // make new array of all notes except the one with ID provided in URL
+      const result = json.filter((note) => note.note_id !== noteId);
+
+      // save that array to filesys
+      writeToFile('./db/db.json', result);
+
+      // respond to DELETE request
+      res.json(`Item ${noteId} has been deleted`);
+    });
+});
+
 
 // POST route for new note
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { noteTitle, noteText } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      noteTitle,
+      noteText,
+      note_id: uuidv4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
 
 
 module.exports = notes;
