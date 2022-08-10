299da6eb2181e261b4ead54f3be18348185f44b0
John Roller <jtroller33@gmail.com>
2022-08-09 21:33:34 -0500

    9:30pm 8.9

--- routes/notes.js
+++ routes/notes.js
@@ -3,11 +3,7 @@
 const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 
-const {
-  readFromFile,
-  readAndAppend,
-  writeToFile,
-} = require('../helpers/fsUtils');
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 
 // GET route for retrieving all the notes
 notes.get('/', (req, res) => {
